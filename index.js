f62653801770633c55f547dea35cffa4c9655458
CShuwen <chenshuwen0210@gmail.com>
2022-05-28 19:38:06 +0800

    add game pag

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const PORT = process.env.PORT || 4000;
 const express = require('express');
+const res = require('express/lib/response');
 const app = express();
 
 app.listen(PORT, () => {
@@ -10,4 +11,9 @@ app.listen(PORT, () => {
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
+
+app.get("/game", (req, res) => {
+    res.sendFile(__dirname + "game.html");
+}
+);
 
